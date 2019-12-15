27e85d384f48516377c4988294046da3ad92ac73
Umesh Dhauni <malkeet@kilobytetech.com>
2019-12-15 23:50:05 +0530

    food lib home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,10 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  img={
+    src:'dish6.svg',
+    index:5
+  };
+  allImages = ['dish1.svg','dish2.svg','dish3.svg','dish4.svg','dish5.svg','dish6.svg']
   constructor() { }
 
   ngOnInit() {
+    this.cycle(0);
+  }
+
+  cycle(i) {
+    
+    setTimeout(() => {
+      this.img = {
+        src:this.allImages[i],
+        index:i
+      }
+      i++;
+      if (i > 5) {
+        i = 0;
+      }
+      this.cycle(i);
+    }, 4000);
+    // }
   }
 
 }
