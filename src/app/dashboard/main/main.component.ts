41fe86ab4042c1fadad3b53248afb1da4348ce16
Umesh Dhauni <mobistreak@mobistreaks-MacBook-Air.local>
2019-12-21 02:43:21 +0530

    category shadow

--- src/app/dashboard/main/main.component.ts
+++ src/app/dashboard/main/main.component.ts
@@ -6,7 +6,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent implements OnInit {
-  allCategories=['dsaf','sdaf','dasf','afs','dsaf','sdaf','dasf','afs']
+  allCategories=[{
+    name:'Pizza',
+    image:'pizza-slice.svg'
+  },
+  {
+    name:'Chinese',
+    image:'noodles.svg'
+  },
+  {
+    name:'Drinks',
+    image:'beer.svg'
+  },
+  {
+    name:'Burger',
+    image:'burgerim.svg'
+  },
+  {
+    name:'Dessert',
+    image:'cupcake.svg'
+  },
+  {
+    name:'Japanese',
+    image:'sushi.svg'
+  },
+  {
+    name:'Barbecue',
+    image:'skewer.svg'
+  }
+];
+allDishes=['','','','']
   constructor() { }
 
   ngOnInit() {
