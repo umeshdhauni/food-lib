6293be6045a0f4561724894eab927859faacd719
Umesh Dhauni <mobistreak@mobistreaks-MacBook-Air.local>
2019-12-21 11:39:51 +0530

    home dashboard design

--- src/app/dashboard/main/main.component.ts
+++ src/app/dashboard/main/main.component.ts
@@ -35,7 +35,26 @@ export class MainComponent implements OnInit {
     image:'skewer.svg'
   }
 ];
-allDishes=['','','','']
+allDishes=[{
+  name:'Egg Curry',
+  image:'best2.svg',
+  price:'40',
+  tag:'Egg Special',
+  rating:5
+},{
+  name:'Hungry Sandwich',
+  image:'best3.svg',
+  price:'50',
+  tag:'Fast Food',
+  rating:3.7
+},
+{
+  name:'Strawberry Pancake',
+  image:'best1.svg',
+  price:'20',
+  tag:'Desserts',
+  rating:4
+},]
   constructor() { }
 
   ngOnInit() {
